9c31c94856e5600688c302d29cc43664acfa228c
caprest <caprest.f@gmail.com>
2017-02-23 07:35:08 +0900

    アルゴリズムのミスを修正

--- machine_learning/data_process.py
+++ machine_learning/data_process.py
@@ -31,77 +31,76 @@ def train_test_split(values):
             train.extend(values_label[0:int(train_num)])
             test.extend(values_label[int(train_num):])
 
-    return train,test
+    return train,test,train_num
 
 
-def parse_an_article(string):
+def make_vocab_from_article(string):
     mc = mecab()
     result = mc.parse(string).split("\n")
     result = [re.split(r"\t|,",parsed_word) for parsed_word in result ]
     result = [word[0] for word in result[:-1] if word[1]=="名詞"]
-    return result
+    vocab = set(result)
+    return vocab
 
-def make_word_number_dict(counters):
+def make_counters(values):
+    #Order of value [title,source,sentence,label]
+    counters = [collections.Counter() for i in range(10)]
+    for value in values:
+        vocab = make_vocab_from_article(value[2])
+        if value[3] ==1:
+            counters[0].update(vocab)
+        elif value[3] ==2:
+            counters[1].update(vocab)
+        elif value[3] ==3:
+            counters[2].update(vocab)
+        elif value[3] ==4:
+            counters[3].update(vocab)
+        elif value[3] ==5:
+            counters[4].update(vocab)
+        elif value[3] ==6:
+            counters[5].update(vocab)
+        elif value[3] ==7:
+            counters[6].update(vocab)
+        elif value[3] ==8:
+            counters[7].update(vocab)
+        elif value[3] ==9:
+            counters[8].update(vocab)
+    for counter in counters:
+        counters[9] += counter
+    return counters
+
+
+def make_word_number_dict(counters,train_num):
     prob_dic={}
     for word,num in counters[9].items():
         word_num = np.ones(9)
         if word in counters[0]:
             word_num[0] += counters[0][word]
-        elif word in counters[1]:
+        if word in counters[1]:
             word_num[1] += counters[1][word]  
-        elif word in counters[2]:
+        if word in counters[2]:
             word_num[2] += counters[2][word]
-        elif word in counters[3]:
+        if word in counters[3]:
             word_num[3] += counters[3][word]
-        elif word in counters[4]:
+        if word in counters[4]:
             word_num[4] += counters[4][word]
-        elif word in counters[5]:
+        if word in counters[5]:
             word_num[5] += counters[5][word]
-        elif word in counters[6]:
+        if word in counters[6]:
             word_num[6] += counters[6][word]
-        elif word in counters[7]:
+        if word in counters[7]:
             word_num[7] += counters[7][word]
-        elif word in counters[8]:
+        if word in counters[8]:
             word_num[8] += counters[8][word]
-        else:
-            print(word)
-            raise Exception("Something is wrong with your dictionary")
-        word_num=np.log(word_num/num)
-        prob_dic[word]=word_num
-
+        prob=np.log(word_num/(train_num+1))
+        prob_dic[word]=prob
     return prob_dic
 
 def save_data(filename,obj):
     with open(filename,"wb") as f:
         pickle.dump(obj,f)
 
-def make_counters(values):
-    #Order of value [title,source,sentence,label]
-    counters = [collections.Counter() for i in range(10)]
-    for value in values:
-        parse_noun = parse_an_article(value[2])
-        if value[3] ==1:
-            counters[0].update(parse_noun)
-        elif value[3] ==2:
-            counters[1].update(parse_noun)
-        elif value[3] ==3:
-            counters[2].update(parse_noun)
-        elif value[3] ==4:
-            counters[3].update(parse_noun)
-        elif value[3] ==5:
-            counters[4].update(parse_noun)
-        elif value[3] ==6:
-            counters[5].update(parse_noun)
-        elif value[3] ==7:
-            counters[6].update(parse_noun)
-        elif value[3] ==8:
-            counters[7].update(parse_noun)
-        elif value[3] ==9:
-            counters[8].update(parse_noun)
 
-    for counter in counters:
-        counters[9] += counter
-    return counters
 
 
 
@@ -113,12 +112,12 @@ if __name__ == '__main__':
     p_data = args.path
     n_data = "".join(p_data.split(".")[:-1])
     header,values = read_data(p_data)
-    train,test = train_test_split(values)
+    train,test,train_num = train_test_split(values)
     counters = make_counters(train)
     save_data("train"+n_data,train)
     save_data("test"+n_data,test)
     if args.split == False:
-        dictionary = make_word_number_dict(counters)
+        dictionary = make_word_number_dict(counters,train_num)
         save_data("dictionary_"+n_data,dictionary)
 
     
